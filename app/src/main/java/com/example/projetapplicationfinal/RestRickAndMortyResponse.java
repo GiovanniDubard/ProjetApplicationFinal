d785124ca99aa0f510ebf3fc4aee425ca1fade03
giova <dubard@et.esiea.fr>
2020-05-09 20:39:49 +0200

    [feature][AppelServeur] Installation de l'API Rick and Morty

---
+++ app/src/main/java/com/example/projetapplicationfinal/RestRickAndMortyResponse.java
@@ -0,0 +1,28 @@
+package com.example.projetapplicationfinal;
+
+import java.util.List;
+
+public class RestRickAndMortyResponse {
+
+    private Integer count;
+    private Integer pages;
+    private String next;
+    private String prev;
+    private List<Characters> results;
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public List<Characters> getResults() {
+        return results;
+    }
+}
