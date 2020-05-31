d2680b90def5d0b1b8c2c0d774a42f1a9934cbeb
giova <dubard@et.esiea.fr>
2020-05-31 17:44:35 +0200

    [feature][MusicBackground] Add Background Music

--- app/src/main/java/com/example/projetapplicationfinal/presentataion/view/MainActivity.java
+++ app/src/main/java/com/example/projetapplicationfinal/presentataion/view/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.projetapplicationfinal.R;
@@ -20,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-
+    private MediaPlayer mediaPlayer;
     private MainController controller;
 
 
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.ricktheme);
         controller = new MainController(
                 this,
                 Singletons.getGson(),
@@ -38,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void playSound(View view){
+    mediaPlayer.start();
+    }
+
+    public void stopSound(View view){
+        mediaPlayer.stop();
+    }
 
     public void showList(List<Characters> charactersList){
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
